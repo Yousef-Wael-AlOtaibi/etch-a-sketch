86ef03eb4346c9b54c1ee538a95dbefa8584f734
Yousef-Wael-AlOtaibi <ywalotaibi2010@gmail.com>
2025-10-31 17:15:01 +0300

    Add better grid structure and more function
    Make stuff function by setting a set width so it
    doesn't grow too much and use javascript
    to have a btn prompt the user to reform the grid.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const grid = document.querySelector('#grid');
+const reformGridBtn = document.querySelector('#reform-grid-btn');
 function colorDiv(event){
     const div = event.target;
     const randomColor = `rgb(${(Math.random()*256)},
@@ -6,8 +7,17 @@ function colorDiv(event){
     ${(Math.random()*256)}`;
     div.style.backgroundColor = randomColor;
 }
-for(let i = 0; i<256; i++){
+function formGrid(squares){
+    grid.replaceChildren('');
+    for(let i = 0; i<squares*squares; i++){
     const div = document.createElement('div');
     div.addEventListener('mouseenter',colorDiv);
     grid.appendChild(div);
 }
+}
+formGrid(16);
+reformGridBtn.addEventListener('click', () => {
+    let squares = +prompt('How many squares per side do you want now? (max 100x100 cuz performance)');
+    if(squares > 100)squares = 100;
+    formGrid(squares);
+})
